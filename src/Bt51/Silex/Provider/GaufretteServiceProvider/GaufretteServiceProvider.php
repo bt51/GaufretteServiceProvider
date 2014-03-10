d33723e7c43956bf67a047846d7998cacc772a8f
Ben Tollakson <ben.tollakson@johnsonfit.com>
2014-03-10 17:30:37 +0000

    updated contact email address

--- src/Bt51/Silex/Provider/GaufretteServiceProvider/GaufretteServiceProvider.php
+++ src/Bt51/Silex/Provider/GaufretteServiceProvider/GaufretteServiceProvider.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of GaufretteServiceProvider
  *
- * (c) Ben Tollakson <ben.tollakson@gmail.com>
+ * (c) Ben Tollakson <btollakson.os@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
